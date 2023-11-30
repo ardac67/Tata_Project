760a5b214b0d11c4f1e80d2edab11536df767d37
terensahin <terensahin2@gmail.com>
2023-11-30 10:36:12 +0300

    campaign router

--- Backend/src/handlers/campaing.ts
+++ Backend/src/handlers/campaing.ts
@@ -1,8 +1,8 @@
-import prisma from '../db'
+import prisma from "../db";
 
 export const createCampagin = async (req, res) => {
   try {
-    await prisma.$transaction(async tx => {
+    await prisma.$transaction(async (tx) => {
       const campaign = await tx.campaign.create({
         data: {
           user_id: req.body.user_id,
@@ -10,18 +10,18 @@ export const createCampagin = async (req, res) => {
           campaign_description: req.body.campaign_description,
           status: req.body.status,
           startedAt: parseAndFormatDate(req.body.campaignStartDate),
-          endedAt: parseAndFormatDate(req.body.campaignEndDate)
-        }
-      })
+          endedAt: parseAndFormatDate(req.body.campaignEndDate),
+        },
+      });
       const collaboration = await tx.collaboration_preferences.create({
         data: {
           target_audience: req.body.target_audience,
           age_interval: req.body.age_interval,
           gender_information: req.body.gender_information,
           statistical_interval: req.body.statistical_interval,
-          campaign_id: campaign.campaign_id
-        }
-      })
+          campaign_id: campaign.campaign_id,
+        },
+      });
       const tags = await tx.campaing_tags.create({
         data: {
           tag1: req.body.tag1,
@@ -29,51 +29,64 @@ export const createCampagin = async (req, res) => {
           tag3: req.body.tag3,
           tag4: req.body.tag4,
           tag5: req.body.tag5,
-          campaign_id: campaign.campaign_id
-        }
-      })
+          campaign_id: campaign.campaign_id,
+        },
+      });
       const preffered_platforms = await tx.preffered_platforms.create({
         data: {
           platform: req.body.platform,
-          preference_id: collaboration.preference_id
-        }
-      })
+          preference_id: collaboration.preference_id,
+        },
+      });
       res.json({
         data: {
           campaign: campaign,
           collaboration: collaboration,
           tags: tags,
-          preffered_platforms: preffered_platforms
-        }
-      })
-    })
+          preffered_platforms: preffered_platforms,
+        },
+      });
+    });
   } catch (e) {
-    console.log(e)
-    res.status(500)
-    res.json({ error: e })
+    console.log(e);
+    res.status(500);
+    res.json({ error: e });
   }
-}
+};
 export const getAllCampaign = async (req, res) => {
   try {
-      const campaign = await prisma.campaign.findMany({
-        where:{
-          user_id:req.params.id
-        }
-      })
-      res.json({
-          campaign: campaign
-      })
+    const campaign = await prisma.campaign.findMany({
+      where: {
+        user_id: req.params.id,
+      },
+    });
+    res.json({
+      campaign: campaign,
+    });
   } catch (e) {
-    console.log(e)
-    res.status(500)
-    res.json({ error: e })
+    console.log(e);
+    res.status(500);
+    res.json({ error: e });
   }
-}
+};
 
 function parseAndFormatDate(dateString) {
   // Parse the date string to a JavaScript Date object
-  const jsDate = new Date(dateString)
-
+  const jsDate = new Date(dateString);
 
   return jsDate;
 }
+
+export const getAll = async (req, res) => {
+  try {
+    const campaign = await prisma.campaign.findMany({});
+    res.json({
+      campaign: campaign,
+    });
+    res.status(200);
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+    res.json({ error: e });
+  }
+};
--- Backend/src/router.ts
+++ Backend/src/router.ts
@@ -1,18 +1,26 @@
-import { Router } from 'express';
-import { body, oneOf } from 'express-validator';
-import { createContact, deleteContact, editContact, getContact } from './handlers/contact';
-import { handleInputError } from './modules/middleware';
-import { getMediaLinks ,updateMedia} from './handlers/mediaLinks';
-import {getUser,updateUser} from './handlers/user'
-import { createCampagin, getAllCampaign } from './handlers/campaing';
+import { Router } from "express";
+import { body, oneOf } from "express-validator";
+import {
+  createContact,
+  deleteContact,
+  editContact,
+  getContact,
+} from "./handlers/contact";
+import { handleInputError } from "./modules/middleware";
+import { getMediaLinks, updateMedia } from "./handlers/mediaLinks";
+import { getUser, updateUser } from "./handlers/user";
+import { createCampagin, getAllCampaign } from "./handlers/campaing";
+import { getAll } from "./handlers/campaing";
 const router = Router();
 
-router.get('/getUser/:id', getUser);
-router.get('/getContact/:id', getContact);
-router.put('/updateUser/:id', updateUser);
-router.put('/updateContact/:id', editContact);
-router.get('/getMediaLinks/:id', getMediaLinks);
-router.put('/updateMediaLinks/:id', updateMedia);
-router.post('/createCampaign', createCampagin);
-router.get('/allCampaigns/:id', getAllCampaign);
+router.get("/getUser/:id", getUser);
+router.get("/getContact/:id", getContact);
+router.put("/updateUser/:id", updateUser);
+router.put("/updateContact/:id", editContact);
+router.get("/getMediaLinks/:id", getMediaLinks);
+router.put("/updateMediaLinks/:id", updateMedia);
+router.post("/createCampaign", createCampagin);
+router.get("/allCampaigns/:id", getAllCampaign);
+router.get("/allCampaigns", getAll);
+
 export default router;
