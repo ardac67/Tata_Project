fb7c9ad1775bb5f01dd6c580c9eccbf1387fa3fb
arda <ardaardac@gmail.com>
2023-12-28 17:35:19 +0300

    messaging

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
-import {checkSchema} from 'express-validator'
+import { checkSchema } from 'express-validator'
 import { protect } from './modules/aut'
 import { createNewUser, signin } from './handlers/user'
 import { handleInputError } from './modules/middleware'
@@ -8,38 +8,66 @@ import { validatorForUser, validatorSign } from './modules/validationSchemas'
 import cors from 'cors'
 import router from './router'
 import multer from 'multer'
+import { Server } from 'socket.io'
+import http from 'http'
+
+
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log(`User connected ${socket.id}`);
+
+  socket.on('join_room', (data) => {
+    socket.join(data.room); // Join the user to a socket room
+    console.log(`user:${socket.id} user_id: ${data.user},user_name: ${data.user_name}, user_room: ${data.room}`);
+  });
+  socket.on('send_message', (data) => {
+    //{ user: user, message: message, room: 'room1' }
+    io.to(data.room).emit('receive_message', data);
+    console.log(`user:${socket.id} user_name: ${data.user}, message: ${data.message}`, data.room);
+  })
+});
+
+server.listen(3002, () => {
+  console.log('Welcome to Tata-MESSAGE')
+})
 
 const corsOptions = {
-    origin: ['http://localhost:3000', 'null'], // Add 'null' as an allowed origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Enable cookies and HTTP authentication if needed
-    optionsSuccessStatus: 204 // HTTP status code to respond with for preflight requests
-};
-app.use(cors(corsOptions));
+  origin: ['http://localhost:3000', 'null'], // Add 'null' as an allowed origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Enable cookies and HTTP authentication if needed
+  optionsSuccessStatus: 204 // HTTP status code to respond with for preflight requests
+}
+app.use(cors(corsOptions))
 app.use(morgan('dev'))
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
-
-app.get('/',(req,res) => {
-    res.json({message:"'Welcome to Tata-API'"})
+app.get('/', (req, res) => {
+  res.json({ message: "'Welcome to Tata-API'" })
 })
 
 //app.post('/createUser',checkSchema(validatorForUser),handleInputError,createNewUser)
-app.post('/createUser',createNewUser)
-app.post('/signin', checkSchema(validatorSign),handleInputError,signin)
-app.use('/api',protect,router)
+app.post('/createUser', createNewUser)
+app.post('/signin', checkSchema(validatorSign), handleInputError, signin)
+app.use('/api', protect, router)
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      // Specify the folder in the root directory where you want to save the photos
-      cb(null, './uploads'); // 'uploads' should match the folder name you created
-    },
-    filename: (req, file, cb) => {
-      // You can customize the filename if needed
-      cb(null, file.originalname);
-    },
-  });
+  destination: (req, file, cb) => {
+    // Specify the folder in the root directory where you want to save the photos
+    cb(null, './uploads') // 'uploads' should match the folder name you created
+  },
+  filename: (req, file, cb) => {
+    // You can customize the filename if needed
+    cb(null, file.originalname)
+  }
+})
 
 export default app
