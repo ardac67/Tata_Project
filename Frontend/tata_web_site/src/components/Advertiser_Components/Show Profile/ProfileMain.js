76b1f70b301789d386edceb64ba9b90aec9b1dad
tarikcelik1 <t.celik2021@gtu.edu.tr>
2024-01-04 14:13:59 +0300

    profile main

--- Frontend/tata_web_site/src/components/Advertiser_Components/Show Profile/ProfileMain.js
+++ Frontend/tata_web_site/src/components/Advertiser_Components/Show Profile/ProfileMain.js
@@ -1,16 +1,84 @@
-import { MDBRow, MDBCol, MDBCardBody, MDBBtn } from "mdb-react-ui-kit";
+import { MDBRow, MDBCol, MDBCardBody, MDBBtn,MDBSpinner } from "mdb-react-ui-kit";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import fetchCollaboration from './fetchCollaboration'
+import { useQuery } from '@tanstack/react-query'
 import defaultImage from "../default.jpg";
+import Cookies from 'universal-cookie'
 import { bufferToBase64 } from "../../../utils";
 
+function formatDate(dateStr) {
+  let date = new Date(dateStr);
+  let year = date.getFullYear();
+  let month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+  let day = String(date.getDate()).padStart(2, "0");
+  let hour = date.getHours();
+  let minutes = String(date.getMinutes()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
+
+function getDate(){
+// Create a new Date object
+var currentDate = new Date();
+
+// Get the current date components
+var year = currentDate.getFullYear();
+var month = currentDate.getMonth() + 1; // Months are zero-based, so add 1
+var day = currentDate.getDate();
+
+// Format the date as a string (you can customize the format as needed)
+var formattedDate = year + '-' + (month < 10 ? '0' : '') + month + '-' + (day < 10 ? '0' : '') + day;
+return formattedDate;
+}
+
 const ProfileMain = ({ user }) => {
+  const navigate = useNavigate();
+  const editButton = () => {
+    navigate("/AccountSettings");
+  };
+  const cookies = new Cookies(null, { path: '/' })
+  const token = cookies.get('token')
+  const {id} = useParams()
+  const result = useQuery(['collaboration',id, token], fetchCollaboration)
+  if (result.isLoading) {
+    return (
+      <MDBSpinner role='status'>
+        <span className='visually-hidden'>Loading...</span>
+      </MDBSpinner>
+    )
+  }
+  var data = result.data.proposal
+  
+  console.log(data)
+  var index =data.length;
+  var ongoing_collaborations = [];
+  var completed_collaborations = [];
+
+  for(var i=0;i<index;i++){
+    var campaignEndDate = formatDate(data[i].belongToCampaign.endedAt)
+    var todayDate = getDate();
+    if(campaignEndDate < todayDate){
+      completed_collaborations.push(data[i])
+    }
+  }
+
+  var num_of_completed_collaborations = completed_collaborations.length;
+
+  for(var i=0;i<index;i++){
+    var campaignEndDate = formatDate(data[i].belongToCampaign.endedAt)
+    var todayDate = getDate();
+    if(campaignEndDate >= todayDate){
+      ongoing_collaborations.push(data[i])
+    }
+  }
+
+  var num_of_ongoing_collaborations = ongoing_collaborations.length;
+
   function parseDateString(dateString) {
     const parsedDate = new Date(dateString);
     return parsedDate;
   }
-  const navigate = useNavigate();
 
   return (
     <MDBCardBody>
@@ -40,6 +108,9 @@ const ProfileMain = ({ user }) => {
               {user.name} @{user.user_name}
             </MDBCol>
             <MDBCol md="4" className="d-flex justify-content-end">
+              <MDBBtn style={{ marginRight: "0px" }} onClick={editButton}>
+                Edit Profile
+              </MDBBtn>
             </MDBCol>
           </MDBRow>
           <MDBRow
@@ -86,8 +157,8 @@ const ProfileMain = ({ user }) => {
             }}
           >
             <MDBCol md="6">
-              <MDBRow>N/A Collaboration Completed</MDBRow>
-              <MDBRow>N/A Ongoing Campaign</MDBRow>
+              <MDBRow style={{color:"#4040a1"}}>{num_of_completed_collaborations} Collaborations Completed</MDBRow>
+              <MDBRow style={{color:"#feb236"}}>{num_of_ongoing_collaborations} Ongoing Collaborations</MDBRow>
             </MDBCol>
             <MDBCol md="6">
               <MDBRow>
--- Frontend/tata_web_site/src/components/Influencer_Components/Account/ViewProfile/ProfileMain.js
+++ Frontend/tata_web_site/src/components/Influencer_Components/Account/ViewProfile/ProfileMain.js
@@ -1,19 +1,87 @@
-import { MDBRow, MDBCol, MDBCardBody, MDBBtn } from "mdb-react-ui-kit";
+import { MDBRow, MDBCol, MDBCardBody, MDBBtn,MDBSpinner } from "mdb-react-ui-kit";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
+import fetchCollaboration from './fetchCollaboration'
+import { useQuery } from '@tanstack/react-query'
 import defaultImage from "../../default.jpg";
+import Cookies from 'universal-cookie'
 import { bufferToBase64 } from "../../../../utils";
 
+function formatDate(dateStr) {
+  let date = new Date(dateStr);
+  let year = date.getFullYear();
+  let month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+  let day = String(date.getDate()).padStart(2, "0");
+  let hour = date.getHours();
+  let minutes = String(date.getMinutes()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
+
+function getDate(){
+// Create a new Date object
+var currentDate = new Date();
+
+// Get the current date components
+var year = currentDate.getFullYear();
+var month = currentDate.getMonth() + 1; // Months are zero-based, so add 1
+var day = currentDate.getDate();
+
+// Format the date as a string (you can customize the format as needed)
+var formattedDate = year + '-' + (month < 10 ? '0' : '') + month + '-' + (day < 10 ? '0' : '') + day;
+return formattedDate;
+}
+
 const ProfileMain = ({ user }) => {
-  function parseDateString(dateString) {
-    const parsedDate = new Date(dateString);
-    return parsedDate;
-  }
   const navigate = useNavigate();
   const editButton = () => {
     navigate("/AccountSettings");
   };
+  const cookies = new Cookies(null, { path: '/' })
+  const token = cookies.get('token')
+  const user_id = cookies.get('user_id')
+  const result = useQuery(['collaboration', user_id, token], fetchCollaboration)
+  if (result.isLoading) {
+    return (
+      <MDBSpinner role='status'>
+        <span className='visually-hidden'>Loading...</span>
+      </MDBSpinner>
+    )
+  }
+
+ console.log("sssssssssss")
+  console.log(result)
+  var data = result.data.proposal
+  var index =data.length;
+  console.log("sayıııı : " + index)
+  var ongoing_collaborations = [];
+  var completed_collaborations = [];
+
+  for(var i=0;i<index;i++){
+    var campaignEndDate = formatDate(data[i].belongToCampaign.endedAt)
+    var todayDate = getDate();
+    if(campaignEndDate < todayDate){
+      completed_collaborations.push(data[i])
+    }
+  }
+
+  var num_of_completed_collaborations = completed_collaborations.length;
+
+  for(var i=0;i<index;i++){
+    var campaignEndDate = formatDate(data[i].belongToCampaign.endedAt)
+    var todayDate = getDate();
+    if(campaignEndDate >= todayDate){
+      ongoing_collaborations.push(data[i])
+    }
+  }
+
+  var num_of_ongoing_collaborations = ongoing_collaborations.length;
+
+  function parseDateString(dateString) {
+    const parsedDate = new Date(dateString);
+    return parsedDate;
+  }
+
   return (
     <MDBCardBody>
       <MDBRow>
@@ -91,8 +159,8 @@ const ProfileMain = ({ user }) => {
             }}
           >
             <MDBCol md="6">
-              <MDBRow>N/A Collaboration Completed</MDBRow>
-              <MDBRow>N/A Ongoing Campaign</MDBRow>
+              <MDBRow style={{color:"#4040a1"}}>{num_of_completed_collaborations} Collaborations Completed</MDBRow>
+              <MDBRow style={{color:"#feb236"}}>{num_of_ongoing_collaborations} Ongoing Collaborations</MDBRow>
             </MDBCol>
             <MDBCol md="6">
               <MDBRow>
