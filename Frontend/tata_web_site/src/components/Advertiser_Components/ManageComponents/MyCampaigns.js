4e2d780f9c20f9a0ce75dfd4d068c0a5ba530f14
tarikcelik1 <t.celik2021@gtu.edu.tr>
2024-01-04 16:29:46 +0300

    Campaign status update option implemented in My campaigns page.

--- Frontend/tata_web_site/src/components/Advertiser_Components/ManageComponents/MyCampaigns.js
+++ Frontend/tata_web_site/src/components/Advertiser_Components/ManageComponents/MyCampaigns.js
@@ -1,11 +1,14 @@
-import { MDBRow, MDBCol, MDBSpinner, MDBBtn } from 'mdb-react-ui-kit'
+import { MDBRow, MDBCol, MDBSpinner, MDBBtn,MDBDropdown,MDBDropdownItem,MDBDropdownToggle,MDBDropdownMenu} from 'mdb-react-ui-kit'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { useQuery } from '@tanstack/react-query'
 import Cookies from 'universal-cookie'
+import { useState } from 'react';
 import DataTable from 'react-data-table-component'
 import fetchCampaigns from '../Fetch/fetchCampaigns'
 import { Link, useNavigate } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify'
+import axios from 'axios'
 function formatDateAndHour(dateStr) {
   let date = new Date(dateStr);
   let year = date.getFullYear();
@@ -17,6 +20,7 @@ function formatDateAndHour(dateStr) {
 }
 
 const MyCampaigns = () => {
+
   const navigate = useNavigate()
   const cookies = new Cookies(null, { path: '/' })
   const id = cookies.get('user_id')
@@ -25,6 +29,32 @@ const MyCampaigns = () => {
   const hashMap = {
     key1: 'value1'
   };
+  const headers = {
+    Authorization: `Bearer ${token}`
+  }
+  const pushStatus = async (id,status) => {
+    console.log(id,status)
+    console.log(newData);
+    var newData = {
+      newStatus: status
+    }
+    axios
+      .put(`http://localhost:3001/api/updateCampaignStatus/${id}`, newData, {
+        headers
+      })
+      .then(response => {
+        console.log(response)
+        toast.success('Succesfully Updated', {
+          position: toast.POSITION.TOP_LEFT
+        })
+      })
+      .catch(error => {
+        toast.warning('Succesfully ERRER', {
+            position: toast.POSITION.TOP_LEFT
+          })
+      })
+  }
+
   if (result.isLoading) {
     return (
       <MDBSpinner role='status'>
@@ -60,8 +90,18 @@ const MyCampaigns = () => {
         {
           when: row => row.status === 'pending',
           style: {
-            backgroundColor: 'black',
-            color: 'white',
+            backgroundColor: 'yellow',
+            color: 'black',
+            '&:hover': {
+              cursor: 'pointer'
+            }
+          }
+        },
+        {
+          when: row => row.status === 'Ended',
+          style: {
+            backgroundColor: 'red',
+            color: 'black',
             '&:hover': {
               cursor: 'pointer'
             }
@@ -71,7 +111,7 @@ const MyCampaigns = () => {
           when: row => row.status === 'Active',
           style: {
             backgroundColor: 'green',
-            color: 'white',
+            color: 'black',
             '&:hover': {
               cursor: 'pointer'
             }
@@ -96,6 +136,7 @@ const MyCampaigns = () => {
       cell: row => (
         <>
           <MDBRow>
+          <ToastContainer></ToastContainer>
             <MDBCol md='6'>
               <MDBBtn size='md'>
                 <FontAwesomeIcon
@@ -107,12 +148,20 @@ const MyCampaigns = () => {
               </MDBBtn>
             </MDBCol>
             <MDBCol md='6'>
-              <MDBBtn size='md'>
-                <FontAwesomeIcon onClick={() => {
-                  navigate(`/details/${row.campaign_id}`)
-                }} icon={faPenToSquare} />
-              </MDBBtn>
-            </MDBCol>
+        <MDBDropdown>
+          <MDBDropdownToggle size='md' color='primary' >
+          <FontAwesomeIcon icon={faPenToSquare}></FontAwesomeIcon> 
+          </MDBDropdownToggle>
+          <MDBDropdownMenu>
+            <MDBDropdownItem link  onClick={() => pushStatus(row.campaign_id, 'Active')}>
+            <i style={{color:"green"}} class="fas fa-circle-check"></i> Ongoing
+            </MDBDropdownItem>
+            <MDBDropdownItem link  onClick={() => pushStatus(row.campaign_id, 'Ended')}>
+            <i style={{color:"red"}} class="fas fa-circle-xmark"></i> End
+            </MDBDropdownItem>
+          </MDBDropdownMenu>
+        </MDBDropdown>
+      </MDBCol>
           </MDBRow>
         </>
       )
@@ -137,7 +186,7 @@ const MyCampaigns = () => {
       >
         All Campaign
       </h1>
-      <p>This Table Shows All Campaigsn</p>
+      <p>This Table Shows All Campaigns</p>
       <MDBRow style={{ border: '1px solid rgb(222 231 249)' }}>
         <DataTable
           columns={columns}
