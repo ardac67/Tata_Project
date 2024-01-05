5946765b863749012d2d4ee2ac577c91967de40c
tarikcelik1 <t.celik2021@gtu.edu.tr>
2024-01-05 10:53:21 +0300

    a

--- Frontend/tata_web_site/src/components/Influencer_Components/Account/ViewProfile/RatingComponent.js
+++ Frontend/tata_web_site/src/components/Influencer_Components/Account/ViewProfile/RatingComponent.js
@@ -4,99 +4,71 @@ import {
   MDBCard,
   MDBCardBody,
   MDBCardHeader,
-  MDBCardText
+  MDBCardText,
+  MDBSpinner
 } from 'mdb-react-ui-kit'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar, faThumbsUp } from '@fortawesome/free-solid-svg-icons'
 import { CardBody } from 'react-bootstrap'
+import Cookies from 'universal-cookie'
+import { useQuery } from '@tanstack/react-query'
+import fetchRatings from '../Fetch/fetchRatings'
+function formatDateAndHour (dateStr) {
+  let date = new Date(dateStr)
+  let year = date.getFullYear()
+  let month = String(date.getMonth() + 1).padStart(2, '0') // Months are 0-indexed
+  let day = String(date.getDate()).padStart(2, '0')
+  let hour = date.getHours()
+  let minutes = String(date.getMinutes()).padStart(2, '0')
+  return `${year}-${month}-${day} ${hour}:${minutes}`
+}
+
 const RatingComponent = () => {
+
+  const cookies = new Cookies(null, { path: '/' })
+  const token = cookies.get('token')
+  const user_id = cookies.get('user_id')
+  const result = useQuery(['rating', user_id, token], fetchRatings)
+  if (result.isLoading) {
+    return (
+      <MDBSpinner role='status'>
+        <span className='visually-hidden'>Loading...</span>
+      </MDBSpinner>
+    )
+  }
+  console.log("asdsadsad", result.data)
   return (
     <div>
-      Showing 1 - 5 out of 31 reviews
-      <MDBCardBody className='square border border-3'>
-        <MDBRow>
-          <MDBCardHeader style={{ fontSize: '15px' }}>
-            📊 Data-Driven Strategies | 🎨 Creative Ads | 💡 Expert Guidance
-          </MDBCardHeader>
-          <MDBCardBody>
-            <MDBCardText style={{ marginTop: '10px' }}>
-              David delivered not only the project goals itself, but went above
-              & beyond while communicating with the team, suggesting changes to
-              the functionality that was incorrectly designed, adding nice
-              touches here and there that were not accounted for, but are
-              aesthetically pleasing, as well as was able to work overtime to
-              meet the deadlines and deliver the product in time.
-            </MDBCardText>
-            <MDBCardText style={{ marginTop: '10px' }}>
-              Tags: #Data-Driven #Strategies #Creative #Ads #Expert #Guidance
-            </MDBCardText>
-            <MDBCardText style={{ marginTop: '10px' }}>
-              Taha Eren S. @tshain 8 days ago
-            </MDBCardText>
+      Showing 1 - {result.data.rating.length} out of {result.data.rating.length} reviews
+      {!result.data.rating.length ? (
+        <h1>No collaborations yet</h1>
+      ) : (
+        result.data.rating.map((rating) => (
+          <MDBCardBody className='square border border-3'>
+            <MDBRow>
+              <MDBCardHeader style={{ fontSize: '15px' }}>
+                📊 {formatDateAndHour(rating.createdAt)}
+              </MDBCardHeader>
+              <MDBCardBody>
+                <MDBCardText style={{ marginTop: '10px' }}>
+                {rating.rating_text}
+                </MDBCardText>
+                <MDBCardText style={{ marginTop: '10px' }}>
+                  Tags: #Data-Driven #Strategies #Creative #Ads #Expert #Guidance
+                </MDBCardText>
+                <MDBCardText style={{ marginTop: '10px' }}>
+                  Taha Eren S. @tshain 8 days ago
+                </MDBCardText>
+              </MDBCardBody>
+              <MDBRow
+                style={{
+                  fontSize: '15px'
+                }}
+              ></MDBRow>
+            </MDBRow>
           </MDBCardBody>
-          <MDBRow
-            style={{
-              fontSize: '15px'
-            }}
-          ></MDBRow>
-        </MDBRow>
-      </MDBCardBody>
-      <MDBCardBody className='square border border-3'>
-        <MDBRow>
-          <MDBCardHeader style={{ fontSize: '15px' }}>
-            📊 Data-Driven Strategies | 🎨 Creative Ads | 💡 Expert Guidance
-          </MDBCardHeader>
-          <MDBCardBody>
-            <MDBCardText style={{ marginTop: '10px' }}>
-              David delivered not only the project goals itself, but went above
-              & beyond while communicating with the team, suggesting changes to
-              the functionality that was incorrectly designed, adding nice
-              touches here and there that were not accounted for, but are
-              aesthetically pleasing, as well as was able to work overtime to
-              meet the deadlines and deliver the product in time.
-            </MDBCardText>
-            <MDBCardText style={{ marginTop: '10px' }}>
-              Tags: #Data-Driven #Strategies #Creative #Ads #Expert #Guidance
-            </MDBCardText>
-            <MDBCardText style={{ marginTop: '10px' }}>
-              Taha Eren S. @tshain 8 days ago
-            </MDBCardText>
-          </MDBCardBody>
-          <MDBRow
-            style={{
-              fontSize: '15px'
-            }}
-          ></MDBRow>
-        </MDBRow>
-      </MDBCardBody>
-      <MDBCardBody className='square border border-3'>
-        <MDBRow>
-          <MDBCardHeader style={{ fontSize: '15px' }}>
-            📊 Data-Driven Strategies | 🎨 Creative Ads | 💡 Expert Guidance
-          </MDBCardHeader>
-          <MDBCardBody>
-            <MDBCardText style={{ marginTop: '10px' }}>
-              David delivered not only the project goals itself, but went above
-              & beyond while communicating with the team, suggesting changes to
-              the functionality that was incorrectly designed, adding nice
-              touches here and there that were not accounted for, but are
-              aesthetically pleasing, as well as was able to work overtime to
-              meet the deadlines and deliver the product in time.
-            </MDBCardText>
-            <MDBCardText style={{ marginTop: '10px' }}>
-              Tags: #Data-Driven #Strategies #Creative #Ads #Expert #Guidance
-            </MDBCardText>
-            <MDBCardText style={{ marginTop: '10px' }}>
-              Taha Eren S. @tshain 8 days ago
-            </MDBCardText>
-          </MDBCardBody>
-          <MDBRow
-            style={{
-              fontSize: '15px'
-            }}
-          ></MDBRow>
-        </MDBRow>
-      </MDBCardBody>
+        ))
+      )}
     </div>
   )
 }
