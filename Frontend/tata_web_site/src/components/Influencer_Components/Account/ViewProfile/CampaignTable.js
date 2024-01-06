93568b5fcfb5635cf96ebedb441e261efbc6a23a
terensahin <terensahin2@gmail.com>
2024-01-06 03:21:48 +0300

    bana ne abi ya

--- Frontend/tata_web_site/src/components/Influencer_Components/Account/ViewProfile/CampaignTable.js
+++ Frontend/tata_web_site/src/components/Influencer_Components/Account/ViewProfile/CampaignTable.js
@@ -1,76 +1,76 @@
-import DataTable from 'react-data-table-component'
-import {
-  MDBRow,
-  MDBSpinner,
+import DataTable from "react-data-table-component";
+import { MDBRow, MDBSpinner } from "mdb-react-ui-kit";
 
-} from 'mdb-react-ui-kit'
-
-import { useQuery } from '@tanstack/react-query'
-import fetchCollaboration from './fetchCollaboration'
-import Cookies from 'universal-cookie'
-import { Link, useNavigate } from 'react-router-dom'
+import { useQuery } from "@tanstack/react-query";
+import fetchCollaboration from "./fetchCollaboration";
+import Cookies from "universal-cookie";
+import { Link, useNavigate } from "react-router-dom";
 const CampaingTable = () => {
-  const navigate = useNavigate()
-  const cookies = new Cookies(null, { path: '/' })
-  const token = cookies.get('token')
-  const user_id = cookies.get('user_id')
-  const result = useQuery(['collaboration', user_id, token], fetchCollaboration)
+  const navigate = useNavigate();
+  const cookies = new Cookies(null, { path: "/" });
+  const token = cookies.get("token");
+  const user_id = cookies.get("user_id");
+  const result = useQuery(
+    ["collaboration", user_id, token],
+    fetchCollaboration
+  );
   if (result.isLoading) {
     return (
-      <MDBSpinner role='status'>
-        <span className='visually-hidden'>Loading...</span>
+      <MDBSpinner role="status">
+        <span className="visually-hidden">Loading...</span>
       </MDBSpinner>
-    )
+    );
   }
-  var data = result.data.proposal
+  var data = result.data.proposal;
 
   function formatDateAndHour(dateStr) {
-    let date = new Date(dateStr)
-    let year = date.getFullYear()
-    let month = String(date.getMonth() + 1).padStart(2, '0') // Months are 0-indexed
-    let day = String(date.getDate()).padStart(2, '0')
-    let hour = date.getHours()
-    let minutes = String(date.getMinutes()).padStart(2, '0')
-    return `${year}-${month}-${day} ${hour}:${minutes}`
+    let date = new Date(dateStr);
+    let year = date.getFullYear();
+    let month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+    let day = String(date.getDate()).padStart(2, "0");
+    let hour = date.getHours();
+    let minutes = String(date.getMinutes()).padStart(2, "0");
+    return `${year}-${month}-${day} ${hour}:${minutes}`;
   }
-  
-  
-const columns = [
-  {
-    name: 'Campaign Header',
-    selector: row => row.belongToCampaign.campaign_header  // Corrected: Now this just returns the campaign header.
 
-  },
-  {
-    name: 'Start Date',
-    selector: row => formatDateAndHour(row.belongToCampaign.startedAt)
-  },
-  {
-    name: 'End Date',
-    selector: row => formatDateAndHour(row.belongToCampaign.endedAt)
-  }
- 
-]
+  const columns = [
+    {
+      name: "Campaign Header",
+      selector: (row) => (
+        <Link to={`/CampaignDetails/${row.belongToCampaign.campaign_id}`}>
+          {row.belongToCampaign.campaign_header}
+        </Link>
+      ),
+    },
+    {
+      name: "Start Date",
+      selector: (row) => formatDateAndHour(row.belongToCampaign.startedAt),
+    },
+    {
+      name: "End Date",
+      selector: (row) => formatDateAndHour(row.belongToCampaign.endedAt),
+    },
+  ];
 
-return (
-  <div
-    style={{
-      marginTop: '15px',
-      marginBottom: '15px',
-      marginLeft: '5%',
-      marginRight: '5%'
-    }}
-  >
-    <MDBRow style={{ border: '1px solid rgb(222 231 249)' }}>
-      <DataTable
-        columns={columns}
-        data={data}
-        pagination
-        highlightOnHover
-        pointerOnHover
-      />
-    </MDBRow>
-  </div>
-)
-}
-export default CampaingTable
+  return (
+    <div
+      style={{
+        marginTop: "15px",
+        marginBottom: "15px",
+        marginLeft: "5%",
+        marginRight: "5%",
+      }}
+    >
+      <MDBRow style={{ border: "1px solid rgb(222 231 249)" }}>
+        <DataTable
+          columns={columns}
+          data={data}
+          pagination
+          highlightOnHover
+          pointerOnHover
+        />
+      </MDBRow>
+    </div>
+  );
+};
+export default CampaingTable;
--- Frontend/tata_web_site/src/components/Influencer_Components/Show Profile/CampaignTable.js
+++ Frontend/tata_web_site/src/components/Influencer_Components/Show Profile/CampaignTable.js
@@ -1,21 +1,21 @@
-import DataTable from 'react-data-table-component'
-import { MDBRow, MDBCol, MDBSpinner, MDBBtn } from 'mdb-react-ui-kit'
-import { useQuery } from '@tanstack/react-query'
-import Cookies from 'universal-cookie'
-import fetchCampaigns from './fetchCampaigns'
-import { Link, useNavigate, useParams } from 'react-router-dom'
+import DataTable from "react-data-table-component";
+import { MDBRow, MDBCol, MDBSpinner, MDBBtn } from "mdb-react-ui-kit";
+import { useQuery } from "@tanstack/react-query";
+import Cookies from "universal-cookie";
+import fetchCampaigns from "./fetchCampaigns";
+import { Link, useNavigate, useParams } from "react-router-dom";
 const CampaingTable = () => {
-  const navigate = useNavigate()
-  const cookies = new Cookies(null, { path: '/' })
-  const {id} = useParams()
-  const token = cookies.get('token')
-  const result = useQuery(['campaign', id, token], fetchCampaigns)
+  const navigate = useNavigate();
+  const cookies = new Cookies(null, { path: "/" });
+  const { id } = useParams();
+  const token = cookies.get("token");
+  const result = useQuery(["campaign", id, token], fetchCampaigns);
   if (result.isLoading) {
     return (
-      <MDBSpinner role='status'>
-        <span className='visually-hidden'>Loading...</span>
+      <MDBSpinner role="status">
+        <span className="visually-hidden">Loading...</span>
       </MDBSpinner>
-    )
+    );
   }
   function formatDateAndHour(dateStr) {
     let date = new Date(dateStr);
@@ -26,85 +26,88 @@ const CampaingTable = () => {
     let minutes = String(date.getMinutes()).padStart(2, "0");
     return `${year}-${month}-${day} ${hour}:${minutes}`;
   }
-  const campaigns = result.data.campaign
+  const campaigns = result.data.campaign;
 
-  const data = []
+  const data = [];
   for (var i = 0; i < campaigns.length; i++) {
-    data.push(campaigns[i])
+    data.push(campaigns[i]);
   }
 
+  const columns = [
+    {
+      name: "Campaign ID",
+      selector: (row) => (
+        <Link to={`/CampaignDetails/${row.campaign_id}`}>
+          {row.campaign_id}
+        </Link>
+      ),
+    },
+    {
+      name: "Campaign Header",
+      selector: (row) => row.campaign_header, // Corrected: Now this just returns the campaign header.
+      cell: (
+        row // 'cell' is used in react-data-table-component for custom render
+      ) => (
+        <Link to={`/CampaignDetails/${row.campaign_id}`}>
+          {row.campaign_header}
+        </Link> // Display the link with the campaign header
+      ),
+    },
+    {
+      name: "Status",
+      selector: (row) => row.status,
+      conditionalCellStyles: [
+        {
+          when: (row) => row.status === "pending",
+          style: {
+            backgroundColor: "yellow",
+            color: "black",
+            "&:hover": {
+              cursor: "pointer",
+            },
+          },
+        },
+        {
+          when: (row) => row.status === "Active",
+          style: {
+            backgroundColor: "green",
+            color: "white",
+            "&:hover": {
+              cursor: "pointer",
+            },
+          },
+        },
+      ],
+    },
+    {
+      name: "Start Date",
+      selector: (row) => formatDateAndHour(row.startedAt),
+    },
+    {
+      name: "End Date",
+      selector: (row) => formatDateAndHour(row.endedAt),
+    },
+  ];
 
-
-const columns = [
-  {
-    name: 'Campaign ID',
-    selector: row => (
-      <Link to={`/details/${row.campaign_id}`}>{row.campaign_id}</Link>
-    )
-  },
-  {
-    name: 'Campaign Header',
-    selector: row => row.campaign_header,  // Corrected: Now this just returns the campaign header.
-    cell: row => (  // 'cell' is used in react-data-table-component for custom render
-      <Link to={`/CampaignDetails/${row.campaign_id}`}>{row.campaign_header}</Link>  // Display the link with the campaign header
-    )
-  },
-  {
-    name: 'Status',
-    selector: row => row.status,
-    conditionalCellStyles: [
-      {
-        when: row => row.status === 'pending',
-        style: {
-          backgroundColor: 'yellow',
-          color: 'black',
-          '&:hover': {
-            cursor: 'pointer'
-          }
-        }
-      },
-      {
-        when: row => row.status === 'Active',
-        style: {
-          backgroundColor: 'green',
-          color: 'white',
-          '&:hover': {
-            cursor: 'pointer'
-          }
-        }
-      }
-    ]
-  },
-  {
-    name: 'Start Date',
-    selector: row => formatDateAndHour(row.startedAt)
-  },
-  {
-    name: 'End Date',
-    selector: row => formatDateAndHour(row.endedAt)
-  }
- 
-]
-
-return (
-  <div
-    style={{
-      marginTop: '15px',
-      marginBottom: '15px',
-      marginLeft: '5%',
-      marginRight: '5%'
-    }}
-  >
-    <MDBRow style={{ border: '1px solid rgb(222 231 249)' }}>
-      <DataTable
-        columns={columns}
-        data={data}
-        pagination
-        highlightOnHover
-        pointerOnHover
-      />
-    </MDBRow>
-  </div>
-)
-}
-export default CampaingTable
+  return (
+    <div
+      style={{
+        marginTop: "15px",
+        marginBottom: "15px",
+        marginLeft: "5%",
+        marginRight: "5%",
+      }}
+    >
+      <MDBRow style={{ border: "1px solid rgb(222 231 249)" }}>
+        <DataTable
+          columns={columns}
+          data={data}
+          pagination
+          highlightOnHover
+          pointerOnHover
+        />
+      </MDBRow>
+    </div>
+  );
+};
+export default CampaingTable;
